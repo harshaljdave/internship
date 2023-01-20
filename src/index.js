8e6754e89774c890d5854bcd8ac19d25d6219c76
harshaljdave <harshaldave5121@gmail.com>
2023-01-20 13:48:17 +0530

    Task-1

--- src/index.js
+++ src/index.js
@@ -1,14 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Routes, Route, NavLink,Link } from 'react-router-dom';
+
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import T1q2 from './task_1/T1q2';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Router>
+    <div className='Navdiv'>
+      <ul className='Navul'>
+        <li>
+          <NavLink to="/">Home</NavLink>
+        </li>
+        <li>
+          <Link to="/task_1/T1q2">T1Q2</Link>
+        </li>
+        {/* <li>
+          <NavLink to="/mcode">Morse code converter</NavLink>
+        </li> */}
+      </ul>
+    </div>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/task_1/T1q2" element={<T1q2 />} />
+      {/* <Route path='/mcode' element={<MorseCode />} /> */}
+      <Route path='*' element={<h1>Error 404</h1>} />
+    </Routes>
+  </Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/task_1/T1q2.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function T1q2() {
+  return (
+    <div style={{ textAlign:"center", fontSize:"xx-large", backgroundColor:"lightgray",padding:"10px 0" }}>
+        Rendering a different component instead of the Deafult App component
+    </div>
+  )
+}
+
+export default T1q2
